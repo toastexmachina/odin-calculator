f03ebf3f90b3f4fec5dd7f939d5ea6fcc54f60a4
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-08-21 10:47:34 -0400

    Define operate function and operation variables

--- script.js
+++ script.js
@@ -10,5 +10,19 @@
 // Operate function
 // takes an operator and 2 numbers and produces an outcome 
 let operate = function (x, y, z){
-    if (z = )
+    if (z === add){
+        return x+y;
+    } else if (z === subtract) {
+        return x-y;
+    } else if (z === multiply) {
+        return x*y;
+    } else if (z === divide) {
+        return x/y;
+    }
 }
+
+// Define operation variables
+const add = document.querySelector('#add');
+const subtract = document.querySelector('#subtract');
+const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
