5320f7c9d466af587233c5d27949b2646ab6c7c6
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-08-21 10:55:19 -0400

    Define number button variables

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ const add = document.querySelector('#add');
 const subtract = document.querySelector('#subtract');
 const multiply = document.querySelector('#multiply');
 const divide = document.querySelector('#divide');
+
+// Define button variables
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+
+// Button Event Listeners -> display text content
