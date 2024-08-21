a1767400d9f30e9aa8991ae343ccee980738b449
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-08-21 10:25:12 -0400

    Outline objectives and pseudocode

---
+++ script.js
@@ -0,0 +1,13 @@
+// Calculator JS
+
+/*
+- operate function
+- number buttons show up on display
+- operations
+- results show up on display
+*/
+
+// Operate function
+let operate = function {
+    
+}
