008da1ed4474b84af80bf9b9e185ffdb893a6353
toastexmachina <156368225+toastexmachina@users.noreply.github.com>
2024-08-22 12:06:36 -0400

    Add event listeners for single-number operations
    - Set up individual event listeners for each operation to set the
    operator parameter in the operate function.
    - Added event listener for the entire operation class to handle first
    entry with a preset second entry.
    - Note: Need to implement a method to store the second entry in the
    entry2 parameter.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 // Calculator JS
 
-// Operate function
-// takes an operator and 2 numbers and produces an outcome 
-let operate = function (x, y, z){
-    if (z === add){
-        return x+y;
-    } else if (z === subtract) {
-        return x-y;
-    } else if (z === multiply) {
-        return x*y;
-    } else if (z === divide) {
-        return x/y;
-    }
-}
-
 const display = document.querySelector('#display');
 const equal = document.querySelector(".equal");
 const clear = document.querySelector(".clear");
@@ -38,6 +24,8 @@ const seven = document.querySelector('#seven');
 const eight = document.querySelector('#eight');
 const nine = document.querySelector('#nine');
 
+/* ——————————————————————————————————————————————————— */
+
 let array = [];
 
 // Adds event listener to every number button
@@ -55,37 +43,63 @@ clear.addEventListener("click", function(){
     display.textContent = array.join('').toString(); 
 })
 
+// OPERATIONS
+
+// Operate function
+// takes an operator and 2 numbers and produces an outcome 
+
+let operate = function (x, y, z){
+    if (z === add){
+        return x+y;
+    } else if (z === subtract) {
+        return x-y;
+    } else if (z === multiply) {
+        return x*y;
+    } else if (z === divide) {
+        return x/y;
+    }
+}
+
+
+let entry1;
+let entry2;
+let operator;
+
+add.addEventListener('click', function(){
+    operator = add; 
+})
+subtract.addEventListener('click', function(){
+    operator = subtract; 
+})
+divide.addEventListener('click', function(){
+    operator = divide;
+})
+multiply.addEventListener('click', function(){
+    operator = multiply;
+})
+
+operation.forEach(function(operatorID){
+    operatorID.addEventListener('click', function(){
+        entry1 = Number(display.textContent);
+        array = [];
+        entry2 = 10; // NEXT TIME! Figure out how to store entry2
+        display.textContent = operate(entry1, entry2, operator);
+    });
+})
+
+
 /*
-button.addEventListener("click", function(){
-    display.textContent = array.join('').toString();
-});
-*/
-
-// button click adds the number to the end
-// build an array that automatically displays?
-// let array = ["1", '2', '3'];
-
-// display.textContent = array.join('').toString();
-
+- Operation OR equals event listener 
+    -> store displayed value as entry2 value 
+    -> store operator as operation value
+    -> execute operation(entry1, entry2, operation)
+    */
 /* 
 PLAN LOOK HERE FIRST
-- Build text display
-
-
-
 - Operation event listener -> store displayed value as entry1 value
 - Build new text display
 - Operation OR equals event listener 
     -> store displayed value as entry2 value 
     -> store operator as operation value
     -> execute operation(entry1, entry2, operation)
-*/
-
-
-
-/*
-let entry1 = display.textContent;
-let entry2 = 6;
-let operation = subtract;
-display.textContent = operate(entry1, entry2, operation).toString();
 */
